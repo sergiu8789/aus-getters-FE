5e8776dbd22280a0fe6ec2c11df669b8a718b43f
parkerappwrk <parker.s@appwrk.com>
2023-10-10 11:04:20 +0530

    font family added

--- src/app/login/page.js
+++ src/app/login/page.js
@@ -1,5 +1,6 @@
 import React from "react";
 import styles from "./login.module.css";
+import Image from "next/image";
 import Logo from "../../../public/assets/images/Logo.png";
 import LoginBg from "../../../public/assets/images/login_background.png";
 
@@ -9,17 +10,19 @@ export default function page() {
       <div
         className={`${styles.loginContainer} position-fixed col-12 d-inline-flex align-items-stretch start-0 top-0 end-0 bottom-0`}
       >
-        <div className={`${styles.LogoBox} d-inline-block`}>
-          <img src={Logo} alt="Logo"></img>
+        <div className={`${styles.LogoBox} position-absolute d-inline-block`}>
+          <Image src={Logo} className="object-fit-contain" alt="Logo"></Image>
         </div>
         <div className="col-md-6">
-          <img
+          <Image
             src={LoginBg}
             alt="Login Bg"
             className="object-fit-cover col-12 d-inline-block h-100"
           />
         </div>
-        <div className="col-md-6 p-5"></div>
+        <div className="col-md-6 p-5">
+          
+        </div>
       </div>
     </React.Fragment>
   );
