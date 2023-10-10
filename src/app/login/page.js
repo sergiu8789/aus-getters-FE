1d203bca9879d444fb5f665c6f195f75bb5bdfa4
parkerappwrk <parker.s@appwrk.com>
2023-10-10 10:42:15 +0530

    login page css changes

--- src/app/login/page.js
+++ src/app/login/page.js
@@ -1,13 +1,17 @@
 import React from "react";
 import styles from "./login.module.css";
+import Logo from "../../../public/assets/images/Logo.png";
 import LoginBg from "../../../public/assets/images/login_background.png";
 
-export default page = () => {
+export default function page() {
   return (
     <React.Fragment>
       <div
-        className={`${styles.loginContainer} position-fixed col-12 d-inline-block start-0 top-0 end-0 bottom-0`}
+        className={`${styles.loginContainer} position-fixed col-12 d-inline-flex align-items-stretch start-0 top-0 end-0 bottom-0`}
       >
+        <div className={`${styles.LogoBox} d-inline-block`}>
+          <img src={Logo} alt="Logo"></img>
+        </div>
         <div className="col-md-6">
           <img
             src={LoginBg}
@@ -15,8 +19,8 @@ export default page = () => {
             className="object-fit-cover col-12 d-inline-block h-100"
           />
         </div>
-        <div className="col-md-6"></div>
+        <div className="col-md-6 p-5"></div>
       </div>
     </React.Fragment>
   );
-};
+}
