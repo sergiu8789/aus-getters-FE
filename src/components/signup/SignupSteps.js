fc0eef1367fb21e29b2072326a3e245c2479ffb4
parkerappwrk <parker.s@appwrk.com>
2023-10-11 11:01:23 +0530

    signup step1 complete

--- src/components/signup/SignupSteps.js
+++ src/components/signup/SignupSteps.js
@@ -5,14 +5,27 @@ import { Formik } from 'formik';
 import backArrow from '../../../public/assets/images/backArrow.svg';
 import documnet from '../../../public/assets/images/upload-documnet.png';
 import profile from '../../../public/assets/images/upload-profile.png';
+import nextArrow from '../../../public/assets/images/nextArrow.svg';
 
-const StepHeader = ({ title }) => {
+const StepHeader = ({ title, step }) => {
   return (
     <React.Fragment>
       <div className={`col-12 d-inline-flex flex-column`}>
         <h2 className={`${styles.stepTitle} text-center`}>{title}</h2>
         <div
-          className={`${styles.stepBlocks} ${styles.activeStep1} position-relative overflow-hidden col-12 d-inline-flex align-items-center justify-content-between`}
+          className={`${styles.stepBlocks} ${
+            step === '1'
+              ? styles.activeStep1
+              : step === '2'
+              ? styles.activeStep2
+              : step === '3'
+              ? styles.activeStep3
+              : step === '4'
+              ? styles.activeStep4
+              : step === '5'
+              ? styles.activeStep5
+              : ''
+          } position-relative overflow-hidden col-12 d-inline-flex align-items-center justify-content-between`}
         >
           <div
             className={`${styles.stepCircle} ${styles.stepCircle1} d-inline-block position-relative`}
@@ -45,7 +58,7 @@ const Step1 = () => {
           className={`${styles.registerContent} col-12 d-flex h-100 align-items-center m-auto`}
         >
           <div className={`${styles.registerModel} overflow-y-auto col-12`}>
-            <StepHeader title="Fill in some basic details" />
+            <StepHeader title="Fill in some basic details" step="1" />
             <div
               className={`${styles.backStep} d-inline-flex align-items-center gap-2`}
               role="button"
@@ -205,6 +218,224 @@ const Step1 = () => {
                 </form>
               )}
             </Formik>
+            <div className="col-12 d-inline-flex align-items-center justify-content-between">
+              <span
+                className={`${styles.skipBtn} d-inline-flex align-items-center`}
+                role="button"
+              >
+                Skip
+              </span>
+              <span
+                className={`${styles.nextBtn} d-inline-flex align-items-center`}
+                role="button"
+              >
+                Next
+              </span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </React.Fragment>
+  );
+};
+
+const Step2 = () => {
+  return (
+    <React.Fragment>
+      <div
+        className={`${styles.registerPopup} position-fixed h-100 col-12 d-inline-block start-0 top-0`}
+      >
+        <div
+          className={`${styles.registerContent} col-12 d-flex h-100 align-items-center m-auto`}
+        >
+          <div className={`${styles.registerModel} overflow-y-auto col-12`}>
+            <StepHeader title="You Skills matter!" step="2" />
+            <div
+              className={`${styles.backStep} d-inline-flex align-items-center gap-2`}
+              role="button"
+            >
+              <Image src={backArrow} alt="Back" />
+              <span className={styles.backLinkText}>Back</span>
+            </div>
+            <Formik
+              initialValues={{
+                image: profile,
+                age: '',
+                yourself: '',
+                resume: documnet,
+                letter: documnet
+              }}
+              validate={(values) => {
+                const errors = {};
+                return errors;
+              }}
+              onSubmit={(values, { setSubmitting }) => {
+                setTimeout(() => {
+                  alert(JSON.stringify(values, null, 2));
+                  setSubmitting(false);
+                }, 400);
+              }}
+            >
+              {({
+                values,
+                errors,
+                touched,
+                handleChange,
+                handleBlur,
+                handleSubmit,
+                isSubmitting
+                /* and other goodies */
+              }) => (
+                <form
+                  className={`${styles.loginFromBox} col-12 d-inline-flex flex-column`}
+                  onSubmit={handleSubmit}
+                >
+                  <div
+                    className={`${styles.loginFormFloating} col-12 position-relative d-inline-flex align-items-center gap-4`}
+                  >
+                    <div
+                      className={`${styles.loginFormBox} col-6 flex-shrink-1 position-relative d-inline-block`}
+                    >
+                      <select
+                        onChange={handleChange}
+                        onBlur={handleBlur}
+                        className={`${styles.formSelect} d-inline-block col-12`}
+                        role="button"
+                      >
+                        <option value="CSS">CSS</option>
+                        <option value="HTML">HTML</option>
+                        <option value="PHP">PHP</option>
+                        <option value="REACTJS">REACTJS</option>
+                      </select>
+                    </div>
+                    <div
+                      className={`${styles.loginFormBox} col-6 flex-shrink-1 position-relative d-inline-block`}
+                    >
+                      <select
+                        onChange={handleChange}
+                        onBlur={handleBlur}
+                        className={`${styles.formSelect} d-inline-block col-12`}
+                        role="button"
+                      >
+                        <option value="CSS">CSS</option>
+                        <option value="HTML">HTML</option>
+                        <option value="PHP">PHP</option>
+                        <option value="REACTJS">REACTJS</option>
+                      </select>
+                    </div>
+                  </div>
+                  <div
+                    className={`${styles.loginFormFloating} col-12 position-relative d-inline-flex align-items-center gap-4`}
+                  >
+                    <div
+                      className={`${styles.loginFormBox} col-6 flex-shrink-1 position-relative d-inline-block`}
+                    >
+                      <select
+                        onChange={handleChange}
+                        onBlur={handleBlur}
+                        className={`${styles.formSelect} d-inline-block col-12`}
+                        role="button"
+                      >
+                        <option value="CSS">CSS</option>
+                        <option value="HTML">HTML</option>
+                        <option value="PHP">PHP</option>
+                        <option value="REACTJS">REACTJS</option>
+                      </select>
+                    </div>
+                    <div
+                      className={`${styles.loginFormBox} col-6 flex-shrink-1 position-relative d-inline-block`}
+                    >
+                      <select
+                        onChange={handleChange}
+                        onBlur={handleBlur}
+                        className={`${styles.formSelect} d-inline-block col-12`}
+                        role="button"
+                      >
+                        <option value="CSS">CSS</option>
+                        <option value="HTML">HTML</option>
+                        <option value="PHP">PHP</option>
+                        <option value="REACTJS">REACTJS</option>
+                      </select>
+                    </div>
+                  </div>
+                </form>
+              )}
+            </Formik>
+          </div>
+        </div>
+      </div>
+    </React.Fragment>
+  );
+};
+
+const Step3 = () => {
+  return (
+    <React.Fragment>
+      <div
+        className={`${styles.registerPopup} position-fixed h-100 col-12 d-inline-block start-0 top-0`}
+      >
+        <div
+          className={`${styles.registerContent} col-12 d-flex h-100 align-items-center m-auto`}
+        >
+          <div className={`${styles.registerModel} overflow-y-auto col-12`}>
+            <StepHeader title="Add your Work Experience" step="3" />
+            <div
+              className={`${styles.backStep} d-inline-flex align-items-center gap-2`}
+              role="button"
+            >
+              <Image src={backArrow} alt="Back" />
+              <span className={styles.backLinkText}>Back</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </React.Fragment>
+  );
+};
+
+const Step4 = () => {
+  return (
+    <React.Fragment>
+      <div
+        className={`${styles.registerPopup} position-fixed h-100 col-12 d-inline-block start-0 top-0`}
+      >
+        <div
+          className={`${styles.registerContent} col-12 d-flex h-100 align-items-center m-auto`}
+        >
+          <div className={`${styles.registerModel} overflow-y-auto col-12`}>
+            <StepHeader title="Add your Education History" step="4" />
+            <div
+              className={`${styles.backStep} d-inline-flex align-items-center gap-2`}
+              role="button"
+            >
+              <Image src={backArrow} alt="Back" />
+              <span className={styles.backLinkText}>Back</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </React.Fragment>
+  );
+};
+
+const Step5 = () => {
+  return (
+    <React.Fragment>
+      <div
+        className={`${styles.registerPopup} position-fixed h-100 col-12 d-inline-block start-0 top-0`}
+      >
+        <div
+          className={`${styles.registerContent} col-12 d-flex h-100 align-items-center m-auto`}
+        >
+          <div className={`${styles.registerModel} overflow-y-auto col-12`}>
+            <StepHeader title="Lets Get Started" step="5" />
+            <div
+              className={`${styles.backStep} d-inline-flex align-items-center gap-2`}
+              role="button"
+            >
+              <Image src={backArrow} alt="Back" />
+              <span className={styles.backLinkText}>Back</span>
+            </div>
           </div>
         </div>
       </div>
@@ -217,6 +448,7 @@ const SignupSteps = () => {
     <React.Fragment>
       <div className={`${styles.registerLayer} modal-backdrop`}></div>
       <Step1 />
+      <Step2 />
     </React.Fragment>
   );
 };
