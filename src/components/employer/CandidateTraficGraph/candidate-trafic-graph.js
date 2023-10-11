ee80d0fcb7a388c9432e57426a6d85f3502db1d6
Glen <Glen.n@appwrk.com>
2023-10-11 11:05:39 +0530

    WIP

--- src/components/employer/CandidateTraficGraph/candidate-trafic-graph.js
+++ src/components/employer/CandidateTraficGraph/candidate-trafic-graph.js
@@ -1,9 +1,21 @@
 import styles from './candidate-trafic-graph.module.css';
 import Link from 'next/link';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 const CandidateTraficGraphComponent = () => {
   return (
-    <>CandidateTraficGraphComponent</>
+    <div className={`${styles.page_conatainer}`}>
+      <Container>
+        <Row className="gx-3 gy-3">
+          <Col>
+            <div className={`${styles.title}`}>Candidate traffic</div>
+          </Col>
+          <Col>2 of 2</Col>
+        </Row>
+      </Container>
+    </div>
   );
 };
 
