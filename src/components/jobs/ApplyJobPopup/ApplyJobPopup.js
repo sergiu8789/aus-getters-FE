ea7a4bdad7eb5dac17f9dec21522f0641988449f
Arison <arison.s@appwrk.com>
2023-10-12 17:46:22 +0530

    add chnages for edit and text area

--- src/components/jobs/ApplyJobPopup/ApplyJobPopup.js
+++ src/components/jobs/ApplyJobPopup/ApplyJobPopup.js
@@ -7,6 +7,7 @@ import nextArrow from '../../../../public/assets/images/nextArrow.svg';
 import { useFormik } from 'formik';
 import * as Yup from 'yup';
 import pdfIcon from '../../../../public/assets/images/pdf.svg';
+import editIcon from '../../../../public/assets/images/editicon.svg';
 
 const StepHeader = ({ title, step }) => {
   return (
@@ -20,6 +21,8 @@ const StepHeader = ({ title, step }) => {
               : step === '2'
               ? styles.activeStep2
               : step === '3'
+              ? styles.activeStep3
+              : ''
           } position-relative overflow-hidden col-12 d-inline-flex align-items-center justify-content-between`}
         >
           <div
@@ -48,7 +51,7 @@ const Step1 = ({ setRegisterStep }) => {
   const formik = useFormik({
     initialValues: {
       selectedResume: '',
-      moreResumes: []
+      managerMessage: ''
     },
     validationSchema: Yup.object({
       selectedResume: Yup.mixed()
@@ -57,10 +60,10 @@ const Step1 = ({ setRegisterStep }) => {
           'Please select a PDF file.',
           (value) => value && value.type === 'application/pdf'
         )
-        .required('Please select a PDF file.')
+        .required('Please select a PDF file.'),
+      managerMessage: Yup.string()
     }),
     onSubmit: (values) => {
-      // Handle form submission here
       console.log(values);
     }
   });
@@ -94,20 +97,22 @@ const Step1 = ({ setRegisterStep }) => {
               <span className={styles.backLinkText}>Back</span>
             </div>
             <div className={styles.pdfSection}>
-              <div className="col-12 d-flex justify-content-between align-items-center">
+              <div
+                className={`${styles.addSpace} col-12 d-flex justify-content-between align-items-center`}
+              >
                 <div className={styles.resumeHeading}>Select Resume</div>
                 <button
                   type="button"
                   onClick={handleAddMore}
                   className={styles.customBtnPrimary}
                 >
-                  Add More
+                  + Add More
                 </button>
                 <input
                   type="file"
                   ref={fileInputRef}
                   name="selectedResume"
-                  accept=".pdf" // Only allow PDF files
+                  accept=".pdf"
                   className="d-none"
                   onChange={handleFileChange}
                 />
@@ -118,8 +123,8 @@ const Step1 = ({ setRegisterStep }) => {
                     key={index}
                     className={` ${
                       index !== pdfFiles.length - 1
-                        ? `py-2 d-flex align-items-center justify-content-between ${styles.pdfItem}`
-                        : 'py-2 d-flex align-items-center justify-content-between'
+                        ? `py-3 d-flex align-items-center justify-content-between ${styles.pdfItem}`
+                        : 'py-3 d-flex align-items-center justify-content-between'
                     }`}
                   >
                     <div className="d-flex align-items-center gap-2">
@@ -163,25 +168,21 @@ const Step1 = ({ setRegisterStep }) => {
             </div>
             <div className={`${styles.pdfSection} mt-4`}>
               <div className="col-12  align-items-center">
-                <div className={styles.resumeHeading}>
+                <div className={`${styles.resumeHeading} ${styles.addSpace}`}>
                   Message For Hiring Manager
                 </div>
-                <label style={{ fontSize: '12px', fontWeight: 300 }}>
+                <label className={`${styles.mainText} ${styles.addSpace}`}>
                   Let them know why you are a good fit or use this space to add
                   a cover later
                 </label>
-                <div className={`${styles.messageManager}`}>
-                  <p style={{ fontSize: '12px', fontWeight: 500 }}>
-                    I'm a deeply curious Mutlidisciplinary Designer based in
-                    Uruguay with a background in Industrial Design and an early
-                    taste of good design
-                  </p>
-                  <p style={{ fontSize: '10px' }}></p>
-                  <p style={{ fontSize: '12px', fontWeight: 500 }}>
-                    I have experience working in small design studio,
-                    advertising agencies, startups, and big companies, so I can
-                    adapt easily to different environment.
-                  </p>
+                <div className={`${styles.messageManager} `}>
+                  <textarea
+                    className={`d-block ${styles.inputField}  w-100 `}
+                    name="managerMessage"
+                    value={formik.values.managerMessage}
+                    onChange={formik.handleChange}
+                    onBlur={formik.handleBlur}
+                  />
                 </div>
                 <span className={styles.addAttachment} onClick={handleAddMore}>
                   Add attachment
@@ -319,7 +320,7 @@ const Step3 = ({ setRegisterStep }) => {
         className={`${styles.applyContent} col-12 d-flex h-100 align-items-center m-auto`}
       >
         <div className={`${styles.applyModel} overflow-y-auto col-12`}>
-          <StepHeader title="Fill the details" step="2" />
+          <StepHeader title="Fill the details" step="3" />
           <div
             className={`${styles.backStep} d-inline-flex align-items-center gap-2`}
             role="button"
@@ -328,42 +329,111 @@ const Step3 = ({ setRegisterStep }) => {
             <Image src={backArrow} alt="Back" />
             <span className={styles.backLinkText}>Back</span>
           </div>
-          <div className={styles.pdfSection}>
-            <div className={styles.thirdHeading}>Resume</div>
-            <div className="d-flex align-items-center justify-content-between ">
+          <div className={`${styles.pdfSection} ${styles.addSpace} `}>
+            <div className={`${styles.thirdHeading} ${styles.addSpace}`}>
+              Resume
+            </div>
+            <div
+              className={`${styles.addSpace} d-flex align-items-center justify-content-between `}
+            >
               <div className={`inline-flex gap-1 ${styles.pdfName}`}>
                 <span>
                   <Image src={pdfIcon} alt="PDF Icon" width="20" height="20" />
                 </span>
                 <span>{'Product Design Resume.pdf'}</span>
               </div>
-              <a href="">Edit</a>
+              <button className={styles.editCustomBtn}>
+                Edit
+                <span>
+                  <Image src={editIcon} alt="edit" />
+                </span>
+              </button>
             </div>
           </div>
-          <div className={`${styles.pdfSection} mt-4`}>
+          <div className={`${styles.pdfSection} ${styles.addSpace} mt-2`}>
             <div className="col-12  align-items-center">
-              <div className="d-flex align-items-center justify-content-between">
+              <div
+                className={`d-flex align-items-center justify-content-between ${styles.addSpace} mt-2`}
+              >
                 <div className={styles.resumeHeading}>
                   Message For Hiring Manager
                 </div>
-                <a href="">Edit</a>
+                <button className={styles.editCustomBtn}>
+                  Edit
+                  <span>
+                    <Image src={editIcon} alt="edit" />
+                  </span>
+                </button>
               </div>
-              <label style={{ fontSize: '12px', fontWeight: 300 }}>
+              <label className={`${styles.mainText} ${styles.addSpace}`}>
                 Let them know why you are a good fit or use this space to add a
                 cover later
               </label>
-              <div className={`${styles.messageManager}`}>
-                <p style={{ fontSize: '12px', fontWeight: 500 }}>
+              <div className={`${styles.messageManager} ${styles.addSpace}`}>
+                <p className={styles.mainText}>
                   I'm a deeply curious Mutlidisciplinary Designer based in
                   Uruguay with a background in Industrial Design and an early
                   taste of good design
                 </p>
-                <p style={{ fontSize: '10px' }}></p>
-                <p style={{ fontSize: '12px', fontWeight: 500 }}>
+                <p className={styles.mainText}></p>
+                <p className={styles.mainText}>
                   I have experience working in small design studio, advertising
                   agencies, startups, and big companies, so I can adapt easily
                   to different environment.
                 </p>
+              </div>
+            </div>
+          </div>
+          <div className={`${styles.pdfSection}`}>
+            <div
+              className={` ${styles.addSpace} d-flex align-items-center justify-content-between mt-2`}
+            >
+              <div className={styles.resumeHeading}>Questions</div>
+              <button className={styles.editCustomBtn}>
+                Edit
+                <span>
+                  <Image src={editIcon} alt="edit" />
+                </span>
+              </button>
+            </div>
+            <div className={styles.questionField}>
+              <label htmlFor="compensationExpectation">
+                What are your compensation expectations?
+              </label>
+              <div className={`${styles.inputField} my-2`}>
+                <input
+                  className={`d-block ${styles.inputField}  w-100`}
+                  type="text"
+                  id="compensationExpectation"
+                  name="compensationExpectation"
+                  value={'$80,000/yr'}
+                />
+              </div>
+            </div>
+            <div className={styles.questionField}>
+              <label htmlFor="joinDate">How soon can you join?</label>
+              <div className={`${styles.inputField} my-2`}>
+                <input
+                  className={`d-block ${styles.inputField}  w-100`}
+                  type="text"
+                  id="joinDate"
+                  name="joinDate"
+                  value={'In 2 weeks'}
+                />
+              </div>
+            </div>
+            <div className={styles.questionField}>
+              <label htmlFor="portfolioLink">
+                Please add your Portfolio link?*
+              </label>
+              <div className={`${styles.inputField} my-2`}>
+                <input
+                  className={`d-block ${styles.inputField}  w-100`}
+                  type="text"
+                  id="portfolioLink"
+                  name="portfolioLink"
+                  value={'https://portfolio.com'}
+                />
               </div>
             </div>
           </div>
@@ -375,7 +445,7 @@ const Step3 = ({ setRegisterStep }) => {
               type="submit"
               className={`${styles.nextBtn} d-inline-flex align-items-center gap-2 justify-content-center`}
             >
-              Continue <Image src={nextArrow} alt="nextArrow" />
+              Submit <Image src={nextArrow} alt="nextArrow" />
             </button>
           </div>
         </div>
