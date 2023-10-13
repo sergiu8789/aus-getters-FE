6b9af76419ba5e7f99fe53200d95032b26e5e894
Arison <arison.s@appwrk.com>
2023-10-13 18:40:05 +0530

    add pop ups in profile settings

--- src/components/AccountSetting/EduTraining/EduTraining.js
+++ src/components/AccountSetting/EduTraining/EduTraining.js
@@ -1,101 +1,167 @@
-import React from "react";
+import React, { useState } from 'react';
 import styles from './EduTraining.module.css';
-import SuitCase from "../../../../public/assets/images/bag.svg";
-import LocationLine from "../../../../public/assets/images/location_line.svg"
-import Cross from "../../../../public/assets/images/x_close.svg"
-import EditIcon from "../../../../public/assets/images/edit_icon.svg"
-import Image from "next/image";
+import SuitCase from '../../../../public/assets/images/bag.svg';
+import LocationLine from '../../../../public/assets/images/location_line.svg';
+import Cross from '../../../../public/assets/images/x_close.svg';
+import EditIcon from '../../../../public/assets/images/edit_icon.svg';
+import Plus from '../../../../public/assets/images/plus.svg';
+import Image from 'next/image';
+import { AddEduTraining } from '../AddEduTraining/AddEduTraining';
 
 export const EduTraining = () => {
-    return (
-        <React.Fragment>
-            <h6>Education & Training</h6>
-            <div className="row mt-3">
-                <div className="col-lg-6 pb-4">
-                    <div className={`${styles.edu_training_box} row m-0 p-3`}>
-                        <div className="col-lg-6">
-                            <p className="m-0 mb-2">School name<br></br><span>Specialization</span></p>  
-                            <div className={`${styles.location_icon_text} d-flex gap-3`}>
-                                <p className="gap-1 align-items-center m-0 d-flex me-2">
-                                    <Image src={LocationLine} className="" alt location></Image>
-                                    Location
-                                </p>
-                                <p className="gap-1 align-items-center m-0 d-flex me-2">
-                                    <Image src={SuitCase} className="" alt location></Image>                                
-                                    GPA: 3.8
-                                </p>
-                            </div>                      
-                        </div>
-                        <div className={`${styles.edu_edit_btn} col-lg-6 text-end flex-wrap d-flex flex-column align-items-end justify-content-between`}>
-                            <Image src={Cross} className="" alt location></Image> 
-                            <p className="m-0 d-flex gap-1 align-items-center">Edit <Image src={EditIcon} className={`object-fit-cover`} alt="EditIcon"></Image></p>
-                        </div>
-                    </div>
-                </div>
-                <div className="col-lg-6 pb-4">
-                    <div className={`${styles.edu_training_box} row m-0 p-3`}>
-                        <div className="col-lg-6">
-                            <p className="m-0 mb-2">College name<br></br><span>Specialization</span></p>  
-                            <div className={`${styles.location_icon_text} d-flex gap-3`}>
-                                <p className="gap-1 align-items-center m-0 d-flex me-2">
-                                    <Image src={LocationLine} className="" alt location></Image>
-                                    Location
-                                </p>
-                                <p className="gap-1 align-items-center m-0 d-flex me-2">
-                                    <Image src={SuitCase} className="" alt location></Image>                                
-                                    GPA: 3.8
-                                </p>
-                            </div>                      
-                        </div>
-                        <div className={`${styles.edu_edit_btn} col-lg-6 text-end flex-wrap d-flex flex-column align-items-end justify-content-between`}>
-                            <Image src={Cross} className="" alt location></Image> 
-                            <p className="m-0 d-flex gap-1 align-items-center">Edit <Image src={EditIcon} className={`object-fit-cover`} alt="EditIcon"></Image></p>
-                        </div>
-                    </div>
-                </div>
-                <div className="col-lg-6 pb-4">
-                    <div className={`${styles.edu_training_box} row m-0 p-3`}>
-                        <div className="col-lg-6">
-                            <p className="m-0 mb-2">School name<br></br><span>Specialization</span></p>  
-                            <div className={`${styles.location_icon_text} d-flex gap-3`}>
-                                <p className="gap-1 align-items-center m-0 d-flex me-2">
-                                    <Image src={LocationLine} className="" alt location></Image>
-                                    Location
-                                </p>
-                                <p className="gap-1 align-items-center m-0 d-flex me-2">
-                                    <Image src={SuitCase} className="" alt location></Image>                                
-                                    GPA: 3.8
-                                </p>
-                            </div>                      
-                        </div>
-                        <div className={`${styles.edu_edit_btn} col-lg-6 text-end flex-wrap d-flex flex-column align-items-end justify-content-between`}>
-                            <Image src={Cross} className="" alt location></Image> 
-                            <p className="m-0 d-flex gap-1 align-items-center">Edit <Image src={EditIcon} className={`object-fit-cover`} alt="EditIcon"></Image></p>
-                        </div>
-                    </div>
-                </div>
-                <div className="col-lg-6 pb-4">
-                    <div className={`${styles.edu_training_box} row m-0 p-3`}>
-                        <div className="col-lg-6">
-                            <p className="m-0 mb-2">College name<br></br><span>Specialization</span></p>  
-                            <div className={`${styles.location_icon_text} d-flex gap-3`}>
-                                <p className="gap-1 align-items-center m-0 d-flex me-2">
-                                    <Image src={LocationLine} className="" alt location></Image>
-                                    Location
-                                </p>
-                                <p className="gap-1 align-items-center m-0 d-flex me-2">
-                                    <Image src={SuitCase} className="" alt location></Image>                                
-                                    GPA: 3.8
-                                </p>
-                            </div>                      
-                        </div>
-                        <div className={`${styles.edu_edit_btn} col-lg-6 text-end flex-wrap d-flex flex-column align-items-end justify-content-between`}>
-                            <Image src={Cross} className="" alt location></Image> 
-                            <p className="m-0 d-flex gap-1 align-items-center">Edit <Image src={EditIcon} className={`object-fit-cover`} alt="EditIcon"></Image></p>
-                        </div>
-                    </div>
-                </div>
+  const [showPopUp, setShowPopup] = useState(false);
+  return (
+    <React.Fragment>
+      <h6>Education & Training</h6>
+      <div className="row mt-3">
+        <div className="col-lg-6 pb-4">
+          <div className={`${styles.edu_training_box} row m-0 p-3`}>
+            <div className="col-lg-6">
+              <p className="m-0 mb-2">
+                School name<br></br>
+                <span>Specialization</span>
+              </p>
+              <div className={`${styles.location_icon_text} d-flex gap-3`}>
+                <p className="gap-1 align-items-center m-0 d-flex me-2">
+                  <Image src={LocationLine} className="" alt location></Image>
+                  Location
+                </p>
+                <p className="gap-1 align-items-center m-0 d-flex me-2">
+                  <Image src={SuitCase} className="" alt location></Image>
+                  GPA: 3.8
+                </p>
+              </div>
             </div>
-        </React.Fragment>
-    )
-}
+            <div
+              className={`${styles.edu_edit_btn} col-lg-6 text-end flex-wrap d-flex flex-column align-items-end justify-content-between`}
+            >
+              <Image src={Cross} className="" alt location></Image>
+              <p className="m-0 d-flex gap-1 align-items-center">
+                Edit{' '}
+                <Image
+                  src={EditIcon}
+                  className={`object-fit-cover`}
+                  alt="EditIcon"
+                ></Image>
+              </p>
+            </div>
+          </div>
+        </div>
+        <div className="col-lg-6 pb-4">
+          <div className={`${styles.edu_training_box} row m-0 p-3`}>
+            <div className="col-lg-6">
+              <p className="m-0 mb-2">
+                College name<br></br>
+                <span>Specialization</span>
+              </p>
+              <div className={`${styles.location_icon_text} d-flex gap-3`}>
+                <p className="gap-1 align-items-center m-0 d-flex me-2">
+                  <Image src={LocationLine} className="" alt location></Image>
+                  Location
+                </p>
+                <p className="gap-1 align-items-center m-0 d-flex me-2">
+                  <Image src={SuitCase} className="" alt location></Image>
+                  GPA: 3.8
+                </p>
+              </div>
+            </div>
+            <div
+              className={`${styles.edu_edit_btn} col-lg-6 text-end flex-wrap d-flex flex-column align-items-end justify-content-between`}
+            >
+              <Image src={Cross} className="" alt location></Image>
+              <p className="m-0 d-flex gap-1 align-items-center">
+                Edit{' '}
+                <Image
+                  src={EditIcon}
+                  className={`object-fit-cover`}
+                  alt="EditIcon"
+                ></Image>
+              </p>
+            </div>
+          </div>
+        </div>
+        <div className="col-lg-6 pb-4">
+          <div className={`${styles.edu_training_box} row m-0 p-3`}>
+            <div className="col-lg-6">
+              <p className="m-0 mb-2">
+                School name<br></br>
+                <span>Specialization</span>
+              </p>
+              <div className={`${styles.location_icon_text} d-flex gap-3`}>
+                <p className="gap-1 align-items-center m-0 d-flex me-2">
+                  <Image src={LocationLine} className="" alt location></Image>
+                  Location
+                </p>
+                <p className="gap-1 align-items-center m-0 d-flex me-2">
+                  <Image src={SuitCase} className="" alt location></Image>
+                  GPA: 3.8
+                </p>
+              </div>
+            </div>
+            <div
+              className={`${styles.edu_edit_btn} col-lg-6 text-end flex-wrap d-flex flex-column align-items-end justify-content-between`}
+            >
+              <Image src={Cross} className="" alt location></Image>
+              <p className="m-0 d-flex gap-1 align-items-center">
+                Edit{' '}
+                <Image
+                  src={EditIcon}
+                  className={`object-fit-cover`}
+                  alt="EditIcon"
+                ></Image>
+              </p>
+            </div>
+          </div>
+        </div>
+        <div className="col-lg-6 pb-4">
+          <div className={`${styles.edu_training_box} row m-0 p-3`}>
+            <div className="col-lg-6">
+              <p className="m-0 mb-2">
+                College name<br></br>
+                <span>Specialization</span>
+              </p>
+              <div className={`${styles.location_icon_text} d-flex gap-3`}>
+                <p className="gap-1 align-items-center m-0 d-flex me-2">
+                  <Image src={LocationLine} className="" alt location></Image>
+                  Location
+                </p>
+                <p className="gap-1 align-items-center m-0 d-flex me-2">
+                  <Image src={SuitCase} className="" alt location></Image>
+                  GPA: 3.8
+                </p>
+              </div>
+            </div>
+            <div
+              className={`${styles.edu_edit_btn} col-lg-6 text-end flex-wrap d-flex flex-column align-items-end justify-content-between`}
+            >
+              <Image src={Cross} className="" alt location></Image>
+              <p className="m-0 d-flex gap-1 align-items-center">
+                Edit
+                <Image
+                  src={EditIcon}
+                  className={`object-fit-cover`}
+                  alt="EditIcon"
+                ></Image>
+              </p>
+            </div>
+          </div>
+        </div>
+        <div className="text-end mb-1">
+          <button
+            role="button"
+            className={`${styles.add_skill_btn} col-12 d-inline-block`}
+            onClick={() => {
+              setShowPopup(!showPopUp);
+            }}
+          >
+            <Image src={Plus} className="" width="10" alt="plus"></Image> Add
+          </button>
+        </div>
+      </div>
+      <AddEduTraining
+        onShow={showPopUp}
+        onClose={() => setShowPopup(!showPopUp)}
+      />
+    </React.Fragment>
+  );
+};
--- src/components/AccountSetting/Resume/Resume.js
+++ src/components/AccountSetting/Resume/Resume.js
@@ -1,36 +1,70 @@
-import React from "react";
-import styles from "./Resume.module.css";
-import Cross from "../../../../public/assets/images/x_close.svg"
-import EditIcon from "../../../../public/assets/images/edit_icon.svg"
-import Location from "../../../../public/assets/images/location_line.svg"
-import Image from "next/image";
-import Plus from "../../../../public/assets/images/plus.svg"
+import React, { useState } from 'react';
+import styles from './Resume.module.css';
+import Cross from '../../../../public/assets/images/x_close.svg';
+import EditIcon from '../../../../public/assets/images/edit_icon.svg';
+import Location from '../../../../public/assets/images/location_line.svg';
+import Image from 'next/image';
+import Plus from '../../../../public/assets/images/plus.svg';
+import { UploadResume } from '../UploadResume/UploadResume';
 
 export const Resume = () => {
-    return (
-        <React.Fragment>
-            <div className={`${styles.resume_container} p-3 py-4`}>
-                <div className="d-flex justify-content-between pb-2">
-                    <h6>Resume</h6>
-                    <p className={`${styles.edu_edit_btn} m-0 d-flex gap-1 align-items-center`}>Edit <Image src={EditIcon} className={`object-fit-cover`} alt="EditIcon"></Image></p>                    
-                </div>
-                <div className={`${styles.single_resume_box} p-3 mb-3 d-flex justify-content-between align-items-center`}>
-                    <p className="m-0">Resume v1.pdf<br></br><span><Image src={Location} className="pe-1" alt="location"></Image>Uploaded: 25 May 2023</span></p>
-                    <Image src={Cross} className="" alt="location"></Image>  
-                </div>
-                <div className={`${styles.single_resume_box} p-3 mb-3 d-flex justify-content-between align-items-center`}>
-                    <p className="m-0">Resume v1.pdf<br></br><span><Image src={Location} className="pe-1" alt="location"></Image>Uploaded: 25 May 2023</span></p>
-                    <Image src={Cross} className="" alt="location"></Image>  
-                </div>
-                <div className="text-end mb-1">
-                    <button role="button" className={`${styles.add_skill_btn} col-12 d-inline-block`}>
-                        <Image src={Plus} className="" width="10" alt="plus"></Image> Add
-                    </button>
-                </div>
-            </div>
-
-            
-            
-        </React.Fragment>
-    )
-}
+  const [showPopUp, setShowPopup] = useState(false);
+  return (
+    <React.Fragment>
+      <div className={`${styles.resume_container} p-3 py-4`}>
+        <div className="d-flex justify-content-between pb-2">
+          <h6>Resume</h6>
+          <p
+            className={`${styles.edu_edit_btn} m-0 d-flex gap-1 align-items-center`}
+          >
+            Edit{' '}
+            <Image
+              src={EditIcon}
+              className={`object-fit-cover`}
+              alt="EditIcon"
+            ></Image>
+          </p>
+        </div>
+        <div
+          className={`${styles.single_resume_box} p-3 mb-3 d-flex justify-content-between align-items-center`}
+        >
+          <p className="m-0">
+            Resume v1.pdf<br></br>
+            <span>
+              <Image src={Location} className="pe-1" alt="location"></Image>
+              Uploaded: 25 May 2023
+            </span>
+          </p>
+          <Image src={Cross} className="" alt="location"></Image>
+        </div>
+        <div
+          className={`${styles.single_resume_box} p-3 mb-3 d-flex justify-content-between align-items-center`}
+        >
+          <p className="m-0">
+            Resume v1.pdf<br></br>
+            <span>
+              <Image src={Location} className="pe-1" alt="location"></Image>
+              Uploaded: 25 May 2023
+            </span>
+          </p>
+          <Image src={Cross} className="" alt="location"></Image>
+        </div>
+        <div className="text-end mb-1">
+          <button
+            role="button"
+            className={`${styles.add_skill_btn} col-12 d-inline-block`}
+            onClick={() => {
+              setShowPopup(!showPopUp);
+            }}
+          >
+            <Image src={Plus} className="" width="10" alt="plus"></Image> Add
+          </button>
+        </div>
+      </div>
+      <UploadResume
+        onShow={showPopUp}
+        onClose={() => setShowPopup(!showPopUp)}
+      />
+    </React.Fragment>
+  );
+};
