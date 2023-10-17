572f5bd1f33c05a93f9d2c677aa206e2af57f637
parkerappwrk <parker.s@appwrk.com>
2023-10-17 12:23:16 +0530

    about page working

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
-function Page() {
-  return <div>page</div>;
+export default function Page() {
+  return <React.Fragment>
+    
+  </React.Fragment>;
 }
-
-export default Page;
