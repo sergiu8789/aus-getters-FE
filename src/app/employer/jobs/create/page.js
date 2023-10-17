8b97d2fa30535da8f7523e14415e4b9c184f200c
Glen <Glen.n@appwrk.com>
2023-10-17 12:07:32 +0530

    Work on post a job page

--- src/app/employer/jobs/create/page.js
+++ src/app/employer/jobs/create/page.js
@@ -1,7 +1,12 @@
-"use client"
+'use client';
 import styles from './job_create.module.css';
 import EmployerLayout from '@/components/employer/Layout/layout';
-import React, { useState } from 'react';
+import BasicDetailStep from './basic_details_step';
+import JobDetailsComponent from './job_details_step';
+import JobLeftSectionComponent from './left_section';
+
+import SelectComponent from '@/components/employer/Select/select';
+import React, { useEffect, useState } from 'react';
 
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
@@ -10,11 +15,20 @@ import Container from 'react-bootstrap/Container';
 import Image from 'next/image';
 import BlueHyphonImg from '../../../../../public/assets/images/blue_hyphon.png';
 import PageTitleComponent from '@/components/employer/PageTitle/page_title';
+
 const JobCreatePage = () => {
-    const [activeStep,setActiveStep] = useState(3);
+  const [activeStep, setActiveStep] = useState(1);
+  const [basicDetails, setBasicDetails] = useState({});
+
+  useEffect(() => {
+    console.log(basicDetails);
+  }, [basicDetails]);
+
+  
   return (
     <>
       <EmployerLayout>
+        {/* <SelectComponent/> */}
         {/* page title */}
         <PageTitleComponent pageTitle={'Post a Job'} />
         <div className={`${styles.page_content}`}>
@@ -22,58 +36,20 @@ const JobCreatePage = () => {
             <Row className="gx-3 gy-3">
               {/* left section */}
               <Col md={3}>
-                <div className={`${styles.left_section}`}>
-                  <div className={`${styles.section1}`}>
-                    {/* Basic Details Step */}
-                    <div className={`${styles.step_item}`}>
-                    {activeStep==1 && <span className={`${styles.hyphon}`}>
-                        {' '}
-                        <Image
-                          src={BlueHyphonImg}
-                          className={``}
-                          alt="Applicants"
-                        ></Image>
-                      </span>}
-                      <span className={`${styles.step_name}`}>
-                        Basic Details
-                      </span>
-                    </div>
-                    {/* Job Details Step */}
-                    <div className={`${styles.step_item}`}>
-                      {activeStep==2 && <span className={`${styles.hyphon}`}>
-                        {' '}
-                        <Image
-                          src={BlueHyphonImg}
-                          className={``}
-                          alt="Applicants"
-                        ></Image>
-                      </span>}
-                      <span className={`${styles.step_name} `+ (activeStep==2?`${styles.step_name_active} `:"")}>
-                        Job Details
-                      </span>
-                    </div>
-                    {/* Question for Candidate Step */}
-                    <div className={`${styles.step_item}`}>
-                      {activeStep==3 && <span className={`${styles.hyphon}`}>
-                        {' '}
-                        <Image
-                          src={BlueHyphonImg}
-                          className={``}
-                          alt="Applicants"
-                        ></Image>
-                      </span>}
-                      <span className={`${styles.step_name} `+ (activeStep==3?`${styles.step_name_active} `:"")}>
-                      Question for Candidate
-                      </span>
-                    </div>
-                  </div>
-                  <div className={`${styles.section2}`}>
-                  Step of <span className={`${styles.current_step}`}>{activeStep}</span> of 3
-                  </div>
-                </div>
+                <JobLeftSectionComponent activeStep={activeStep} />
               </Col>
               {/* Right section */}
-              <Col md={9}></Col>
+              <Col md={9}>
+                {/* form section start here */}
+                {activeStep == 1 ? (
+                  <BasicDetailStep
+                    basicDetails={basicDetails}
+                    setBasicDetails={setBasicDetails}
+                  />
+                ) : (
+                  <JobDetailsComponent/>
+                )}
+              </Col>
             </Row>
           </Container>
         </div>
---
+++ src/components/employer/Select/select.js
@@ -0,0 +1,88 @@
+import React, { useState } from 'react';
+import Select from 'react-select';
+
+const options = [
+  { value: 'chocolate', label: 'Chocolate' },
+  { value: 'strawberry', label: 'Strawberry' },
+  { value: 'vanilla', label: 'Vanilla' }
+];
+
+
+
+  
+const Checkbox = ({ children, ...props }) => (
+    <label style={{ marginRight: '1em' }}>
+      <input type="checkbox" {...props} />
+      {children}
+    </label>
+  );
+const SelectComponent = () => {
+  const [isClearable, setIsClearable] = useState(true);
+  const [isSearchable, setIsSearchable] = useState(true);
+  const [isDisabled, setIsDisabled] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+  const [isRtl, setIsRtl] = useState(false);
+
+  const colourOptions = [
+    { value: 'Red', label: 'Red' },
+    { value: 'Green', label: 'Green' },
+    { value: 'Blue', label: 'Blue' }
+  ];
+  return (
+    <>
+      <Select
+        className="basic-single"
+        classNamePrefix="select"
+        defaultValue={colourOptions[0]}
+        isDisabled={isDisabled}
+        isLoading={isLoading}
+        isClearable={isClearable}
+        isRtl={isRtl}
+        isSearchable={isSearchable}
+        name="color"
+        options={colourOptions}
+      />
+      {/* <Select options={options} /> */}
+
+      <div
+        style={{
+          color: 'hsl(0, 0%, 40%)',
+          display: 'inline-block',
+          fontSize: 12,
+          fontStyle: 'italic',
+          marginTop: '1em',
+        }}
+      >
+        <Checkbox
+          checked={isClearable}
+          onChange={() => setIsClearable((state) => !state)}
+        >
+          Clearable
+        </Checkbox>
+        <Checkbox
+          checked={isSearchable}
+          onChange={() => setIsSearchable((state) => !state)}
+        >
+          Searchable
+        </Checkbox>
+        <Checkbox
+          checked={isDisabled}
+          onChange={() => setIsDisabled((state) => !state)}
+        >
+          Disabled
+        </Checkbox>
+        <Checkbox
+          checked={isLoading}
+          onChange={() => setIsLoading((state) => !state)}
+        >
+          Loading
+        </Checkbox>
+        <Checkbox checked={isRtl} onChange={() => setIsRtl((state) => !state)}>
+          RTL
+        </Checkbox>
+      </div>
+    </>
+  );
+};
+
+export default SelectComponent;
